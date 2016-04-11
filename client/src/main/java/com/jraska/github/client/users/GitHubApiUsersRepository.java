57b9fa68e4d482ed5eb20577698928552f9a1d81
Josef Raska <josef.raska@gmail.com>
2016-04-11 23:58:39 +0100

    Moving Translating user detail to separate method

--- client/src/main/java/com/jraska/github/client/users/GitHubApiUsersRepository.java
+++ client/src/main/java/com/jraska/github/client/users/GitHubApiUsersRepository.java
@@ -1,21 +1,13 @@
 package com.jraska.github.client.users;
 
 import android.support.annotation.NonNull;
-import android.support.v4.util.Pair;
+import android.util.Pair;
 import rx.Observable;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
-import static java.util.Locale.ENGLISH;
+import java.util.ArrayList;
+import java.util.List;
 
 final class GitHubApiUsersRepository implements UsersRepository {
-  static final DateFormat GIT_HUB_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", ENGLISH);
-  static final Comparator<GitHubRepo> BY_STARS_REPO_COMPARATOR = (lhs, rhs) -> rhs.stargazersCount.compareTo(lhs.stargazersCount);
-  static final int MAX_REPOS_TO_DISPLAY = 10;
-
   private final GitHubUsersApi _gitHubUsersApi;
   private final GitHubUserDetailApi _gitHubUserDetailApi;
 
@@ -32,7 +24,7 @@ final class GitHubApiUsersRepository implements UsersRepository {
   @Override public Observable<UserDetail> getUserDetail(String login) {
     return _gitHubUserDetailApi.getUserDetail(login)
         .zipWith(_gitHubUserDetailApi.getRepos(login), Pair::new)
-        .map(result -> translateUserDetail(result.first, result.second));
+        .compose(UserDetailWithReposTranslator.INSTANCE);
   }
 
   List<User> translateUsers(List<GitHubUser> gitHubUsers) {
@@ -47,44 +39,5 @@ final class GitHubApiUsersRepository implements UsersRepository {
   private User translateUser(GitHubUser gitHubUser) {
     boolean isAdmin = gitHubUser.siteAdmin == null ? false : gitHubUser.siteAdmin;
     return new User(gitHubUser.login, gitHubUser.avatarUrl, isAdmin, gitHubUser.htmlUrl);
-  }
-
-  UserDetail translateUserDetail(GitHubUserDetail gitHubUserDetail, List<GitHubRepo> gitHubRepos) {
-    Date joined = parseDate(gitHubUserDetail.createdAt);
-
-    UserStats stats = new UserStats(gitHubUserDetail.followers, gitHubUserDetail.following,
-        gitHubUserDetail.publicRepos, joined);
-
-    Collections.sort(gitHubRepos, BY_STARS_REPO_COMPARATOR);
-
-    List<Repo> usersRepos = new ArrayList<>();
-    List<Repo> contributedRepos = new ArrayList<>();
-
-    for (GitHubRepo gitHubRepo : gitHubRepos) {
-      Repo repo = translateRepo(gitHubRepo);
-      if (gitHubUserDetail.login.equals(gitHubRepo.owner.login)) {
-        usersRepos.add(repo);
-      } else {
-        contributedRepos.add(repo);
-      }
-    }
-
-    return new UserDetail(stats, usersRepos, contributedRepos);
-  }
-
-  Repo translateRepo(GitHubRepo gitHubRepo) {
-    return new Repo(gitHubRepo.name, gitHubRepo.description, gitHubRepo.watchersCount,
-        gitHubRepo.stargazersCount, gitHubRepo.forks, gitHubRepo.size);
-  }
-
-  private static Date parseDate(String text) {
-    synchronized (GIT_HUB_DATE_FORMAT) {
-      try {
-        return GIT_HUB_DATE_FORMAT.parse(text);
-      }
-      catch (ParseException e) {
-        throw new RuntimeException(e); // being lazy now
-      }
-    }
   }
 }
---
+++ client/src/main/java/com/jraska/github/client/users/UserDetailWithReposTranslator.java
@@ -0,0 +1,64 @@
+package com.jraska.github.client.users;
+
+import android.util.Pair;
+import rx.Observable;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.Locale.ENGLISH;
+
+class UserDetailWithReposTranslator
+    implements Observable.Transformer<Pair<GitHubUserDetail, List<GitHubRepo>>, UserDetail> {
+  static final DateFormat GIT_HUB_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", ENGLISH);
+  static final Comparator<GitHubRepo> BY_STARS_REPO_COMPARATOR = (lhs, rhs) -> rhs.stargazersCount.compareTo(lhs.stargazersCount);
+  static final int MAX_REPOS_TO_DISPLAY = 10;
+
+  static final UserDetailWithReposTranslator INSTANCE = new UserDetailWithReposTranslator();
+
+  @Override
+  public Observable<UserDetail> call(Observable<Pair<GitHubUserDetail, List<GitHubRepo>>> observable) {
+    return observable.map(result -> translateUserDetail(result.first, result.second));
+  }
+
+  UserDetail translateUserDetail(GitHubUserDetail gitHubUserDetail, List<GitHubRepo> gitHubRepos) {
+    Date joined = parseDate(gitHubUserDetail.createdAt);
+
+    UserStats stats = new UserStats(gitHubUserDetail.followers, gitHubUserDetail.following,
+        gitHubUserDetail.publicRepos, joined);
+
+    Collections.sort(gitHubRepos, BY_STARS_REPO_COMPARATOR);
+
+    List<Repo> usersRepos = new ArrayList<>();
+    List<Repo> contributedRepos = new ArrayList<>();
+
+    for (GitHubRepo gitHubRepo : gitHubRepos) {
+      Repo repo = translateRepo(gitHubRepo);
+      if (gitHubUserDetail.login.equals(gitHubRepo.owner.login)) {
+        usersRepos.add(repo);
+      } else {
+        contributedRepos.add(repo);
+      }
+    }
+
+    return new UserDetail(stats, usersRepos, contributedRepos);
+  }
+
+  Repo translateRepo(GitHubRepo gitHubRepo) {
+    return new Repo(gitHubRepo.name, gitHubRepo.description, gitHubRepo.watchersCount,
+        gitHubRepo.stargazersCount, gitHubRepo.forks, gitHubRepo.size);
+  }
+
+  private static Date parseDate(String text) {
+    synchronized (GIT_HUB_DATE_FORMAT) {
+      try {
+        return GIT_HUB_DATE_FORMAT.parse(text);
+      }
+      catch (ParseException e) {
+        throw new RuntimeException(e); // being lazy now
+      }
+    }
+  }
+}
