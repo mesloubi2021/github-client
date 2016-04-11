b97c6f4e624eb70359d194386c70795f7d8c8a69
Josef Raska <josef.raska@gmail.com>
2016-04-12 00:49:13 +0100

    Add ok http logging interceptor

--- client/src/main/java/com/jraska/github/client/network/NetworkModule.java
+++ client/src/main/java/com/jraska/github/client/network/NetworkModule.java
@@ -1,13 +1,25 @@
 package com.jraska.github.client.network;
 
+import android.content.Context;
 import com.jraska.github.client.dagger.PerApp;
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import timber.log.Timber;
 
 @Module
 public class NetworkModule {
   @Provides @PerApp OkHttpClient provideOkHttpClient() {
-    return new OkHttpClient();
+    HttpLoggingInterceptor.Logger logger = message -> Timber.tag("OkHttp").d(message);
+    HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(logger);
+    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+
+    OkHttpClient client = new OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build();
+
+    return client;
   }
 }
