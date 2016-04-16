21c7f6f02d7421d3b380b013ca0ebddb120d15ef
Josef Raska <josef.raska@gmail.com>
2016-04-16 18:45:33 +0100

    Hiding repos card if there is no content to display

--- client/src/main/java/com/jraska/github/client/ui/ReposFragment.java
+++ client/src/main/java/com/jraska/github/client/ui/ReposFragment.java
@@ -17,24 +17,36 @@ import java.util.List;
 
 public class ReposFragment extends Fragment {
   @Bind(R.id.repos_recycler) RecyclerView _reposRecyclerView;
+  @Bind(R.id.repos_container) ViewGroup _reposContainer;
 
   @Nullable @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_repos, container, false);
     ButterKnife.bind(this, view);
 
+    setContainerVisibility(false);
     _reposRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
     return view;
   }
 
   void setRepos(List<Repo> repos) {
-    if(_reposRecyclerView == null){
+    if (_reposRecyclerView == null) {
       throw new IllegalStateException("View was not created yet");
     }
+
+    setContainerVisibility(repos.size() > 0);
 
     ReposAdapter reposAdapter = new ReposAdapter(LayoutInflater.from(getActivity()));
     reposAdapter.setRepos(repos);
     _reposRecyclerView.setAdapter(reposAdapter);
+  }
+
+  private void setContainerVisibility(boolean visible) {
+    if (visible) {
+      _reposContainer.setVisibility(View.VISIBLE);
+    } else {
+      _reposContainer.setVisibility(View.GONE);
+    }
   }
 }
