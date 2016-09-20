1716cd81fe4a5ea6ddcdc8ed8487ca6e23cfe301
Josef Raska <josef.raska@gmail.com>
2016-09-20 22:58:57 +0100

    Fix tests and lint fails

--- client/src/test/java/com/jraska/github/client/ui/UsersActivityTest.java
+++ client/src/test/java/com/jraska/github/client/ui/UsersActivityTest.java
@@ -5,17 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class UsersActivityTest {
   @Test
   public void whenCreated_thenUsersRepositoryInjected() throws Exception {
     UsersActivity usersActivity = Robolectric.setupActivity(UsersActivity.class);
 
-    assertThat(usersActivity.usersRepository).isNotNull();
+    assertThat(usersActivity.repository).isNotNull();
   }
 }
