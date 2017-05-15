94de62798c7a4fcc42b3a5f4a63770fc74957f6f
Josef Raska <josef.raska@ocado.com>
2017-05-15 11:25:57 -0700

    Apply PerApp for UsersModule

--- client-data/src/main/java/com/jraska/github/client/data/users/UsersModule.java
+++ client-data/src/main/java/com/jraska/github/client/data/users/UsersModule.java
@@ -1,15 +1,17 @@
 package com.jraska.github.client.data.users;
 
+import com.jraska.github.client.PerApp;
 import com.jraska.github.client.users.UsersRepository;
+
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
 
-import javax.inject.Singleton;
-
 @Module
 public abstract class UsersModule {
-  @Provides public static UsersRepository provideUsersRepository(Retrofit retrofit) {
+  @Provides
+  @PerApp
+  public static UsersRepository provideUsersRepository(Retrofit retrofit) {
     GitHubUsersApi usersApi = retrofit.create(GitHubUsersApi.class);
     GitHubUserDetailApi detailApi = retrofit.create(GitHubUserDetailApi.class);
 
