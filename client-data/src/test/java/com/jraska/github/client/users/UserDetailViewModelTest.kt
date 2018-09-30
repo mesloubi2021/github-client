7657a685ff6886864bfc2823daa3c7517b0049ab
Josef Raska <6277721+jraska@users.noreply.github.com>
2018-10-01 00:39:00 +0200

    Update dependencies (#100)

--- client-data/src/test/java/com/jraska/github/client/users/UserDetailViewModelTest.kt
+++ client-data/src/test/java/com/jraska/github/client/users/UserDetailViewModelTest.kt
@@ -1,45 +1,65 @@
 package com.jraska.github.client.users
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.jraska.github.client.FakeConfig
 import com.jraska.github.client.Navigator
 import com.jraska.github.client.analytics.EventAnalytics
 import com.jraska.github.client.rx.AppSchedulers
+import com.jraska.livedata.test
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.*
 import org.threeten.bp.Instant
 
 class UserDetailViewModelTest {
-  @Test
-  fun whenSameLoginMultipleTimes_thenOnlyOneObservableCreated() {
-    val usersRepository = mock(UsersRepository::class.java)
+  @get:Rule val testRule = InstantTaskExecutorRule()
 
+  lateinit var usersRepository: UsersRepository
+
+  private lateinit var viewModel: UserDetailViewModel
+
+  @Before
+  fun before() {
+    usersRepository = mock(UsersRepository::class.java)
 
     val detailObservable = Observable.just(testDetail())
     `when`(usersRepository.getUserDetail("someLogin", 3)).thenReturn(detailObservable)
     `when`(usersRepository.getUserDetail("different", 3)).thenReturn(detailObservable)
-
     val config = FakeConfig.create(mapOf("user_detail_section_size" to 3L))
 
-    val viewModel = UserDetailViewModel(usersRepository,
+    viewModel = UserDetailViewModel(usersRepository,
       trampoline(), mock(Navigator::class.java), EventAnalytics.EMPTY, config)
+  }
 
+  @Test
+  fun whenLiveData_thenCorrectUser() {
+    viewModel.userDetail("someLogin")
+      .test()
+      .assertHasValue()
+      .assertValue { it.isLoading.not() }
+      .assertValue { it.error() == null }
+  }
+
+  @Test
+  fun whenSameLoginMultipleTimes_thenOnlyOneObservableCreated() {
     val login = "someLogin"
 
-    viewModel.userDetail(login)
+    viewModel.userDetail(login).test().dispose()
     verify(usersRepository).getUserDetail("someLogin", 3)
 
-    viewModel.userDetail(login)
+    viewModel.userDetail(login).test().dispose()
     verify(usersRepository).getUserDetail("someLogin", 3)
 
-    viewModel.userDetail("different")
+    viewModel.userDetail("different").test().dispose()
     verify(usersRepository).getUserDetail("different", 3)
   }
 
   companion object {
     internal fun testDetail(): UserDetail {
-      val user = User("login", "url", true)
+      val user = User("someLogin", "url", true)
       val stats = UserStats(0, 0, 0, Instant.MIN)
       return UserDetail(user, stats, emptyList(), emptyList())
     }
