b79373ede4a259f0aceed8a393a6d1e8013ca879
Josef Raska <6277721+jraska@users.noreply.github.com>
2019-08-04 16:09:58 +0200

    Have shortcuts handled by custom scheme instead of package name (#158)

--- core-android/src/main/java/com/jraska/github/client/core/android/Activity.kt
+++ core-android/src/main/java/com/jraska/github/client/core/android/Activity.kt
@@ -1,10 +1,25 @@
 package com.jraska.github.client.core.android
 
 import android.app.Activity
+import android.net.Uri
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 
 @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 fun Activity.inputUrl(): HttpUrl {
-  return intent.data.toString().toHttpUrl()
+  val inputUri = intent.data ?: throw IllegalArgumentException("No uri provided")
+
+  return replaceNoHttpScheme(inputUri)
+    .toString()
+    .toHttpUrl()
+}
+
+private fun replaceNoHttpScheme(uri: Uri): Uri {
+  if (uri.scheme == "http" || uri.scheme == "https") {
+    return uri
+  }
+
+  return uri.buildUpon()
+    .scheme("https")
+    .build()
 }
