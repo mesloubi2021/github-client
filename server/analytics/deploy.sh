2ad6c7ebc6425f047be5058abf5ad074d07ed0c8
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-12-05 19:32:55 +0100

    Remove cloud function, which was moved into separate repo (#363)

--- server/analytics/deploy.sh
+++
@@ -1,1 +0,0 @@
-gcloud functions deploy github-client-webhooks --entry-point com.jraska.devanalytics.github.GitHubEventsMixpanelFunction --runtime java11 --memory 256MB --trigger-http --allow-unauthenticated
