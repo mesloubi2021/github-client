0894e283bd9306129eb69927c4757718ae92d6cb
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-06-03 09:55:29 +0100

    Add onCreate traces (#529)

--- app/src/main/java/com/jraska/github/client/GitHubClientApp.kt
+++ app/src/main/java/com/jraska/github/client/GitHubClientApp.kt
@@ -1,5 +1,6 @@
 package com.jraska.github.client
 
+import com.google.firebase.perf.metrics.AddTrace
 import com.jraska.github.client.core.android.BaseApp
 import com.jraska.github.client.core.android.HasServiceModelFactory
 import com.jraska.github.client.core.android.HasViewModelFactory
@@ -10,5 +11,10 @@ open class GitHubClientApp : BaseApp(), HasViewModelFactory, HasServiceModelFact
 
   open fun createAppComponent(): AppComponent {
     return DaggerAppComponent.factory().create(this)
+  }
+
+  @AddTrace(name = "GitHubClientApp.onCreate")
+  override fun onCreate() {
+    super.onCreate()
   }
 }
