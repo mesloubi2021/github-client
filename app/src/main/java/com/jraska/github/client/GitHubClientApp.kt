e166ddebc19d161b3f470814b469443f517bcd81
Josef Raska <6277721+jraska@users.noreply.github.com>
2023-05-20 21:01:00 +0200

    Remove Firebase Performance (#889)

--- app/src/main/java/com/jraska/github/client/GitHubClientApp.kt
+++ app/src/main/java/com/jraska/github/client/GitHubClientApp.kt
@@ -1,6 +1,5 @@
 package com.jraska.github.client
 
-import com.google.firebase.perf.metrics.AddTrace
 import com.jraska.github.client.core.android.BaseApp
 import com.jraska.github.client.core.android.HasServiceModelFactory
 import com.jraska.github.client.core.android.HasViewModelFactory
@@ -11,10 +10,5 @@ class GitHubClientApp : BaseApp(), HasViewModelFactory, HasServiceModelFactory {
 
   private fun createAppComponent(): AppComponent {
     return DaggerAppComponent.factory().create(this)
-  }
-
-  @AddTrace(name = "GitHubClientApp.onCreate")
-  override fun onCreate() {
-    super.onCreate()
   }
 }
