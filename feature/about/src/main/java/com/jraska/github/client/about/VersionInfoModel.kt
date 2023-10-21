213b736e7c02562f16e9e069a617d61a1ccf435f
Josef Raska <6277721+jraska@users.noreply.github.com>
2023-10-21 18:42:08 +0200

    Add update info (#956)

--- feature/about/src/main/java/com/jraska/github/client/about/VersionInfoModel.kt
+++ feature/about/src/main/java/com/jraska/github/client/about/VersionInfoModel.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.widget.TextView
 import androidx.core.content.pm.PackageInfoCompat
 import com.airbnb.epoxy.SimpleEpoxyModel
+import java.time.Instant
+import java.time.format.DateTimeFormatter.ISO_INSTANT
 
 class VersionInfoModel : SimpleEpoxyModel(R.layout.about_item_version) {
   override fun bind(view: View) {
@@ -15,8 +17,26 @@ class VersionInfoModel : SimpleEpoxyModel(R.layout.about_item_version) {
     view.findViewById<TextView>(R.id.version_build_type).text = buildTypeText
 
     val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
-    val versionText = context.getString(R.string.about_version_name_and_code, packageInfo.versionName, PackageInfoCompat.getLongVersionCode(packageInfo))
+    val versionText = context.getString(
+      R.string.about_version_name_and_code,
+      packageInfo.versionName,
+      PackageInfoCompat.getLongVersionCode(packageInfo)
+    )
     view.findViewById<TextView>(R.id.version_name_and_code).text = versionText
+
+    val firstInstallTimeText = ISO_INSTANT.format(
+      Instant.ofEpochMilli(packageInfo.firstInstallTime)
+    )
+    val lastUpdateTimeText = ISO_INSTANT.format(
+      Instant.ofEpochMilli(packageInfo.lastUpdateTime)
+    )
+
+    val updateInfoText = context.getString(
+      R.string.about_version_install_info,
+      lastUpdateTimeText,
+      firstInstallTimeText
+    )
+    view.findViewById<TextView>(R.id.version_update_info).text = updateInfoText
   }
 
   override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
